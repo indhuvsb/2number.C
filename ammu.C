3af4faff681eb2d66f4ea081d86451ed1e1db7a3
indhuvsb <36696610+indhuvsb@users.noreply.github.com>
2018-03-21 16:56:27 +0530

    ammu.C

---
+++ ammu.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ 
+
+{
+    while ( n > 0 )
+        n = n - 5;
+ 
+    if ( n == 0 )
+       
+   
+}
+ 
+
+int main()
+{
+    int n = 19;
+    if ( isMultipleof5(n) == true )
+        printf("%d is multiple of 5\n", n);
+  
+    return 0;
+}
